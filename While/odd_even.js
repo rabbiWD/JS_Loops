8a89793b873969c256d3b0e162f1bee636ffaf8d
Rabbi Khan <Krabby2244@gmail.com>
2025-07-31 13:50:20 +0600

    added even number

--- While/odd_even.js
+++ While/odd_even.js
@@ -1,9 +1,17 @@
-let num = 61;
-while(num<=100){
-    console.log(num);
-    if(num%2===1){
+// let num = 61;
+// while(num<=100){
+//     if(num%2===1){
+//         console.log(num);
+//     }
+//     num++;
+
+// }
+
+let num = 78;
+while(num<=98){
+    if(num%2===0){
         console.log(num);
     }
-    num+=2;
+    num++;
 
 }
